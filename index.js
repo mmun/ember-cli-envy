ff15d94c3bd27af9408e8a1bed1ddb06895e1833
Martin Muñoz <im.mmun@gmail.com>
2016-04-29 15:18:28 -0400

    Silence missing .env warnings by default

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-/* jshint node: true */
-'use strict';
-
 var path = require('path');
 var dotenv = require('dotenv');
 var existsSync = require('exists-sync');
+
+function getProjectConfig(project) {
+  var projectConfigPath = path.join(project.root, 'config/envy.js');
+
+  if (existsSync(projectConfigPath)) {
+    return require(projectConfigPath);
+  }
+}
 
 module.exports = {
   name: 'ember-cli-envy',
 
   init: function() {
+    var projectConfig = getProjectConfig(this.project);
+    var config = {};
+
+    // Silence the missing .env warning by default
+    config.silent = true;
+
+    // Inject project configuration
+    for (var key in projectConfig) {
+      config[key] = projectConfig[key];
+    }
+
     // Paths are re-rooted at the project directory instead of the current
     // working directory. This lets `ember` commands work in sub directories.
-
-    var configPath = path.join(this.project.root, 'config/envy.js');
-    var config = existsSync(configPath) ? require(configPath) : {};
-
     config.path = path.join(this.project.root, config.path || '.env');
 
-    dotenv.config(config);
+    dotenv.load(config);
   }
 };
