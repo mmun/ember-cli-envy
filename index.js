f114c988f33bbeab78d3e26119825a118020d1d8
Martin Muñoz <im.mmun@gmail.com>
2016-04-29 15:22:42 -0400

    Small tweak

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var path = require('path');
 var dotenv = require('dotenv');
 var existsSync = require('exists-sync');
 
-function getProjectConfig(project) {
-  var projectConfigPath = path.join(project.root, 'config/envy.js');
+function getProjectConfig(projectRoot) {
+  var projectConfigPath = path.join(projectRoot, 'config/envy.js');
 
   if (existsSync(projectConfigPath)) {
     return require(projectConfigPath);
@@ -14,7 +14,8 @@ module.exports = {
   name: 'ember-cli-envy',
 
   init: function() {
-    var projectConfig = getProjectConfig(this.project);
+    var projectRoot = this.project.root;
+    var projectConfig = getProjectConfig(projectRoot);
     var config = {};
 
     // Silence the missing .env warning by default
@@ -27,7 +28,7 @@ module.exports = {
 
     // Paths are re-rooted at the project directory instead of the current
     // working directory. This lets `ember` commands work in sub directories.
-    config.path = path.join(this.project.root, config.path || '.env');
+    config.path = path.join(projectRoot, config.path || '.env');
 
     dotenv.load(config);
   }
